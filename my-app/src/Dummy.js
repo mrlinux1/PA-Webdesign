af7bffc12f144fd82d3bcf35da548edae56f5750
tsgebert <tsgebert@owncloud.com>
2020-07-04 09:18:47 +0200

    Showing JSON-Object as string

--- my-app/src/Dummy.js
+++ my-app/src/Dummy.js
@@ -4,9 +4,13 @@ import {DrinksContext} from './drinks-context'
 
 export default function Dummy() {
     const [getDrinksData, setDrinksData] = useContext(DrinksContext)
-    return (<h1>Land:
-    {getDrinksData.map((drinks,index) => (<p key={index}>{drinks.country} {drinks.beer_servings} {drinks.spirit_servings} {drinks.wine_servings} {drinks.total_litres_of_pure_alcohol}</p>))}
-    </h1>
+    
+    
+    return (<h1><p>Land: {getDrinksData.beer_servings}</p>
+                <p>Length: {JSON.stringify(getDrinksData)}</p>
+         List:
+        {getDrinksData.map((drinks,index) => (<p key={index}>{drinks.country} {drinks.beer_servings} {drinks.spirit_servings} {drinks.wine_servings} {drinks.total_litres_of_pure_alcohol}</p>))}
+</h1>
     
     )
 }
